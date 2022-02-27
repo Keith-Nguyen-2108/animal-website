f0c22e9c7e30f99db8d904e413604464dc411e10
Keith Nguyen <minhnhakeith@gmail.com>
2022-02-27 23:02:18 +0700

    Upload v2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, BrowserRouter, Switch} from 'react-router-dom'
+
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+
+import Home from './pages/Home'
+import NotFound from './pages/NotFound';
+import Category from './pages/Category';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar/>
+        <Switch>category
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/category">
+            <Category/>
+          </Route>
+
+          <Route component={NotFound} />
+        </Switch>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
